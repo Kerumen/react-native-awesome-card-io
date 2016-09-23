6796eddbbfb6b745bc8f94d5f35a2d7d05fe2521
Yann Pringault <yann@cinq-s.com>
2016-09-23 14:46:34 +0200

    Upgrade card.io iOS SDK to version 5.4.1

--- ios/CardIO/CardIOUtilities.h
+++ ios/CardIO/CardIOUtilities.h
@@ -1,6 +1,6 @@
 //
 //  CardIOUtilities.h
-//  Version 5.3.2
+//  Version 5.4.1
 //
 //  See the file "LICENSE.md" for the full license governing this code.
 //
@@ -35,3 +35,20 @@
 + (UIImageView *)blurredScreenImageView;
 
 @end
+
+/// Methods with names that do not conflict with Apple's private APIs.
+@interface CardIOUtilities (NonConflictingAPINames)
+
+/// Please send the output of this method with any technical support requests.
+/// @return Human-readable version of this library.
++ (NSString *)cardIOLibraryVersion;
+
+/// The preload method prepares card.io to launch faster. Calling preload is optional but suggested.
+/// On an iPhone 5S, for example, preloading makes card.io launch ~400ms faster.
+/// The best time to call preload is when displaying a view from which card.io might be launched;
+/// e.g., inside your view controller's viewWillAppear: method.
+/// preload works in the background; the call to preload returns immediately.
++ (void)preloadCardIO;
+
+@end
+

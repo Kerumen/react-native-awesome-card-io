aff53a783edc264a3364cb519a5d816e305896e5
Diogo Guimaraes <guimaraes@bitreserve.org>
2017-02-13 14:56:22 +0000

    Fix android rn module

--- android/src/main/java/com/cardio/RNCardIOModule.java
+++ android/src/main/java/com/cardio/RNCardIOModule.java
@@ -100,7 +100,7 @@ public class RNCardIOModule extends ReactContextBaseJavaModule implements Activi
   }
 
   @Override
-  public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
     if (requestCode != CARD_IO_SCAN) {
       return;
     }
@@ -120,7 +120,4 @@ public class RNCardIOModule extends ReactContextBaseJavaModule implements Activi
       promise.reject("user_cancelled", "The user cancelled");
     }
   }
-
-  @Override
-  public void onNewIntent(Intent intent) {}
 }

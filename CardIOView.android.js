456cd1c16044f4bccfb1ee6fc9ee355a751def79
Yann Pringault <yann.pringault@gmail.com>
2017-07-31 12:15:07 +0200

    Fix imports in CardIOView.android.js
    Fixes #48

--- CardIOView.android.js
+++ CardIOView.android.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 class CardIOView extends Component {
   render() {

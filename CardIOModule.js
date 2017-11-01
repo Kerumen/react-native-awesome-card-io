536a89b5993964cc6c5da9d7ff12b31ff23c9063
Yann Pringault <yann.pringault@gmail.com>
2017-11-01 20:34:28 +0100

    Add eslint

--- CardIOModule.js
+++ CardIOModule.js
@@ -5,7 +5,7 @@ const RNCardIOModule = NativeModules.CardIOModule;
 const CardIOModule = {
   scanCard(config = {}) {
     return RNCardIOModule.scanCard(config);
-  }
+  },
 };
 
 export default CardIOModule;
--- CardIOView.ios.js
+++ CardIOView.ios.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from  'prop-types';
+import PropTypes from 'prop-types';
 import { requireNativeComponent, NativeAppEventEmitter, NativeModules } from 'react-native';
 
 const { CardIOUtilities } = NativeModules;

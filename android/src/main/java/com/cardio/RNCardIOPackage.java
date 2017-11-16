34cf414c87b237e13653a5c17ea22b202cbac475
rafa <raphael.ribeiro@evivehealth.com>
2017-11-16 11:52:52 +0530

    Removed the method that causes android build to fail.

--- android/src/main/java/com/cardio/RNCardIOPackage.java
+++ android/src/main/java/com/cardio/RNCardIOPackage.java
@@ -20,10 +20,6 @@ public class RNCardIOPackage implements ReactPackage {
       );
     }
 
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
